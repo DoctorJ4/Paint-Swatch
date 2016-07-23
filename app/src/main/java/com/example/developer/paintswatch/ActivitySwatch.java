c96e109e0b4ac39df3a16d63060724fa78a0fa1c
Jesse <jrdodson@pluto.dsu.edu>
2016-07-23 18:58:29 -0500

    1.0

--- app/src/main/java/com/example/developer/paintswatch/ActivitySwatch.java
+++ app/src/main/java/com/example/developer/paintswatch/ActivitySwatch.java
@@ -2,16 +2,22 @@ package com.example.developer.paintswatch;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
 public class ActivitySwatch extends AppCompatActivity {
-    private TextView Rview;
-    private TextView Gview;
-    private TextView Bview;
-    private TextView Aview;
+    private final int max = 255;
+    private final int min = 0;
+    private EditText Rview;
+    private EditText Gview;
+    private EditText Bview;
+    private EditText Aview;
     private Button Bplus;
     private Button Bminus;
     private Button Rplus;
@@ -26,12 +32,15 @@ public class ActivitySwatch extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swatch);
-        View thisView = findViewById(R.id.activity_swatch);
-        //thisView.setBackgroundColor(Color.argb( 255,255,255, 255));
-        Rview = (TextView) findViewById(R.id.RTextView);
-        Gview = (TextView) findViewById(R.id.GTextView);
-        Bview = (TextView) findViewById(R.id.BTextView);
-        Aview = (TextView) findViewById(R.id.ATextView);
+        Rview = (EditText) findViewById(R.id.RTextView);
+        Gview = (EditText) findViewById(R.id.GTextView);
+        Bview = (EditText) findViewById(R.id.BTextView);
+        Aview = (EditText) findViewById(R.id.ATextView);
+        Rview.setFilters(new InputFilter[]{ new InputFilterMinMax("0", "255")});
+        Gview.setFilters(new InputFilter[]{ new InputFilterMinMax("0", "255")});
+        Bview.setFilters(new InputFilter[]{ new InputFilterMinMax("0", "255")});
+        Aview.setFilters(new InputFilter[]{ new InputFilterMinMax("0", "255")});
+
         Bplus = (Button) findViewById(R.id.BPlus);
         Bminus = (Button) findViewById(R.id.BMinus);
         Rplus = (Button) findViewById(R.id.RPlus);
@@ -42,27 +51,55 @@ public class ActivitySwatch extends AppCompatActivity {
         Aminus = (Button) findViewById(R.id.AMinus);
         hideToggle = (ToggleButton) findViewById(R.id.toggleButton);
 
+        setTextChangeListener(Aview);
+        setTextChangeListener(Rview);
+        setTextChangeListener(Gview);
+        setTextChangeListener(Bview);
         setLongListener(Bplus);
         setLongListener(Bminus);
         setLongListener(Rplus);
         setLongListener(Rminus);
         setLongListener(Gplus);
         setLongListener(Gminus);
+        setLongListener(Aplus);
+        setLongListener(Aminus);
 
-        if(hideToggle.getText().equals("off"))
+        if(hideToggle.getText().toString().equals("OFF"))
             setAllVisibility(View.GONE);
+        else
+            setAllVisibility(View.VISIBLE);
 
          changeColor(Rview);
     }
 
     private void setLongListener(View view)
     {
-        view.setOnTouchListener(new RepeatListener(400, 100, new View.OnClickListener() {
+        view.setOnTouchListener(new RepeatListener(400, 10, new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 changeColor(view);
             }
         }));
+    }
+
+    private void setTextChangeListener(final EditText eText)
+    {
+        eText.addTextChangedListener(new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {
+                if(eText.getText().toString().isEmpty() == false)
+                    changeColor(eText);
+            }
+
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+                //Field1.setText("");
+            }
+        });
     }
 
     public void changeColor(View view)
@@ -71,47 +108,63 @@ public class ActivitySwatch extends AppCompatActivity {
         switch (view.getId()) {
             case R.id.BPlus:
                 newValue = (Integer.parseInt(Bview.getText().toString()) + 1);
-                Bview.setText(newValue + "");
+                Bview.setText(safeNewValue(newValue) + "");
                 break;
             case R.id.BMinus:
                 newValue = (Integer.parseInt(Bview.getText().toString()) - 1);
-                Bview.setText(newValue + "");
+                Bview.setText(safeNewValue(newValue) + "");
                 break;
             case R.id.RPlus:
                 newValue = (Integer.parseInt(Rview.getText().toString()) + 1);
-                Rview.setText(newValue + "");
+                Rview.setText(safeNewValue(newValue) + "");
                 break;
             case R.id.RMinus:
                 newValue = (Integer.parseInt(Rview.getText().toString()) - 1);
-                Rview.setText(newValue + "");
+                Rview.setText(safeNewValue(newValue) + "");
                 break;
             case R.id.GPlus:
                 newValue = (Integer.parseInt(Gview.getText().toString()) + 1);
-                Gview.setText(newValue + "");
+                Gview.setText(safeNewValue(newValue) + "");
                 break;
             case R.id.GMinus:
                 newValue = (Integer.parseInt(Gview.getText().toString()) - 1);
-                Gview.setText(newValue + "");
+                Gview.setText(safeNewValue(newValue) + "");
                 break;
             case R.id.APlus:
                 newValue = (Integer.parseInt(Aview.getText().toString()) + 1);
-                Aview.setText(newValue + "");
+                Aview.setText(safeNewValue(newValue) + "");
                 break;
             case R.id.AMinus:
                 newValue = (Integer.parseInt(Aview.getText().toString()) - 1);
-                Aview.setText(newValue + "");
+                Aview.setText(safeNewValue(newValue) + "");
                 break;
         }
 
             findViewById(R.id.activity_swatch).
                     setBackgroundColor(
                             Color.argb(
-                                    Integer.parseInt(Aview.getText().toString()),
-                                    Integer.parseInt(Rview.getText().toString()),
-                                    Integer.parseInt(Gview.getText().toString()),
-                                    Integer.parseInt(Bview.getText().toString())
+                                    Integer.parseInt(safeInt(Aview.getText().toString())),
+                                    Integer.parseInt(safeInt(Rview.getText().toString())),
+                                    Integer.parseInt(safeInt(Gview.getText().toString())),
+                                    Integer.parseInt(safeInt(Bview.getText().toString()))
                             ));
 
+    }
+
+    private String safeInt(String s)
+    {
+        if(s.isEmpty())
+            s = "0";
+        return s;
+    }
+
+    private int safeNewValue(int val)
+    {
+        if(val > max)
+            val = max;
+        else if (val < min)
+            val = min;
+        return val;
     }
 
     public void hideButtons(View view)
